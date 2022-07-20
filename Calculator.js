5550ba459bc584653267ea94c211ce0da2145612
L3OL1N <z3650888@gmail.com>
2022-07-20 15:12:18 +0800

    Feature click display num

--- Calculator.js
+++ Calculator.js
@@ -1,6 +1,23 @@
-var wrapDiv = document.getElementById("wrap");
+const wrapDiv = document.getElementById("wrap");
+const digital = document.getElementById("digital");
+let arrDigital = digital.innerHTML;
 
+function display(e){
+    let num = e.target.innerHTML;
+    let reg = /\d|\./;
+    if(num.match(reg) === null) return;
+    if(arrDigital.match(/\./) != null && num.match(/\./)!= null) return;
+    if(arrDigital === "0"){
+        arrDigital = "";
+    }
+    arrDigital += num;
+    
+    return digital.innerHTML = arrDigital;
+}
+
+//add event listener
 for(let i = 0; i < wrapDiv.childElementCount; i++){
     var id = wrapDiv.children[i].getAttribute("id");
     wrapDiv.children[i].style = "grid-area : "+id;
+    wrapDiv.children[i].addEventListener("click",display);
 };
